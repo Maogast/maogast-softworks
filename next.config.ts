50df977563cd4ffd24f0723555e5859a60a7c68d
Stephen Magare <stevemagare4@gmail.com>
2026-05-20 11:50:26 +0300

    Just add trailingSlash: false to your next.config.ts and redeploy.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  trailingSlash: false,  // 👈 ADD THIS LINE
+
   images: {
     domains: [], // add any external image domains if needed
   },
