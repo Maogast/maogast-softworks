aa8afd1147600fcb3bc7608a5690514a994210f5
Stephen Magare <stevemagare4@gmail.com>
2026-05-26 23:43:55 +0300

    tumbler

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -79,6 +79,7 @@ export default async function ProductPage({ params }: Props) {
               src={product.image}
               alt={product.name}
               fill
+              sizes="(max-width: 768px) 100vw, 50vw"
               className="object-contain"
               priority
             />
